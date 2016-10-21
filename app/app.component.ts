c993d33c4ca67a23ccf868ba7d5e5cb3c554de47
mmalcorn <alcornmeredith@gmail.com>
2016-10-21 18:21:27 -0400

    add model class Food and constructor.  display food name, description, and calories using the ngFor loop

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,26 @@
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'my-app',
   template: `
-  <h1>Meal and Calorie Tracker:</h1>
+  <div class="container">
+    <h1>Meal and Calorie Tracker:</h1>
+    <br><hr>
+    <h3 *ngFor="let currentMeal of meal"><p>Name: {{ currentMeal.foodname }}</p> <p>Description: {{ currentMeal.description }}</p> <p>Calorie Count: {{ currentMeal.calories }}</p><br>
+    </h3>
+  </div>
   `
 })
 
 export class AppComponent {
+  public meal: Food[] = [
+     new Food("Lean Pocket: Philly Cheesesteak", "One Philly Cheesesteak lean pocket", 250, 1),
+     new Food("12 shrimp", "Delicious shrimp that fill me with ecstasy", 300, 2)
+  ];
+}
+
+export class Food {
+  constructor(public foodname: string, public description: string, public calories: number, public id: number) {
+
+  }
 }
