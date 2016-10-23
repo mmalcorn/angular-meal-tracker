6ee496e4f9168bffc7f7548c723bbd296707a49e
mmalcorn <alcornmeredith@gmail.com>
2016-10-23 18:30:32 -0400

    add logic to the food pipe.  business logic that allows high calorie foods and low calorie foods to be filtered

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -4,7 +4,7 @@ import { Food } from './food.model';
 @Component({
   selector: 'food-list',
   template: `
-    <div *ngFor="let currentMeal of childComponentMeal"><p id="foodname"><span id="name">Name:</span> {{ currentMeal.foodname }}</p> <p id="fooddescription"><span id="description">Description:</span> {{ currentMeal.description }}</p> <p id="foodcalories"><span id="calories">Calorie Count:</span> {{ currentMeal.calories }}</p><br>
+    <div *ngFor="let currentMeal of childComponentMeal | calories"><p id="foodname"><span id="name">Name:</span> {{ currentMeal.foodname }}</p> <p id="fooddescription"><span id="description">Description:</span> {{ currentMeal.description }}</p> <p id="foodcalories"><span id="calories">Calorie Count:</span> {{ currentMeal.calories }}</p><br>
       <button class="btn btn-success" (click)="buttonClicked(currentMeal)">Change</button><br>
     </div>
     `
@@ -13,6 +13,7 @@ import { Food } from './food.model';
 export class FoodListComponent {
   @Input() childComponentMeal: Food[];
   @Output() clickSender = new EventEmitter();
+  public selectedCalories: string = "showAll";
   buttonClicked(foodToEdit: Food) {
     this.clickSender.emit(foodToEdit);
   }
--- app/food.pipe.ts
+++ app/food.pipe.ts
@@ -3,14 +3,27 @@ import { Food } from './food.model';
 
 @Pipe({
   name: "calories",
-  pure: true
+  pure: false
 })
 export class FoodPipe implements PipeTransform {
-  transform(input: Food[], args) {
+  transform(input: Food[]) {
     var output: Food[] = [];
+    if (calorieTotal === "highCal") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].calories >= 400) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else if (calorieTotal === "lowCal") {
     for (var i = 0; i < input.length; i++) {
-      
+      if (input[i].calories <= 400) {
+        output.push(input[i]);
+      }
     }
+      return output;
+    } else {
     return input;
+      }
+    }
   }
-}
